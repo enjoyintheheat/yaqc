bf6868125a021f9a00325a2aee79785adadd2c33
enjoyintheheat <enjoyintheheat@gmail.com>
2020-11-16 23:45:49 +0300

    Added new class PipelineController

--- yaqc/__main__.py
+++ yaqc/__main__.py
@@ -2,19 +2,29 @@ import asyncio
 import zmq
 import zmq.asyncio
 from contextlib import suppress
-import sys
 
-if sys.platform == 'win32':
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 ctx = zmq.asyncio.Context()
 
+
+class PipelineController:
+    def __init__(self, max_size=100):
+        self.queue_in = asyncio.Queue(max_size)
+        self.queue_out = asyncio.Queue(max_size)
+
+    async def sub_main_topic(self, name='main', ip_addr='*', port='5555'):
+        self.main_sock = ctx.socket(zmq.SUB)
+        self.main_sock.connect(f'tcp://{ip_addr}:{port}')
+        self.main_sock.setsockopt_string(zmq.SUBSCRIBE, name)
+
+
 async def main():
     sock = ctx.socket(zmq.SUB)
-    sock.connect('tcp://localhost:5555')
-    sock.setsockopt_string(zmq.SUBSCRIBE, '')
+    sock.connect('tcp://*:5555')
+    sock.setsockopt_string(zmq.SUBSCRIBE, '1')
     with suppress(asyncio.CancelledError):
-        while message := await sock.recv_json():
+        while True:
+            message = await sock.recv_json()
             print(message)
     sock.close()
 
