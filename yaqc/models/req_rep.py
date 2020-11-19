0ce603f9b8752dfa65533ea35a90dff3baf422d8
enjoyintheheat <enjoyintheheat@gmail.com>
2020-11-19 22:42:18 +0300

    Added async context manager to RequestZMQ

--- yaqc/models/req_rep.py
+++ yaqc/models/req_rep.py
@@ -9,25 +9,46 @@ ctx = zmq.asyncio.Context()
 class RequestZMQ(SocketZMQ):
     def __init__(self, ip_addr='127.0.0.1', port='5555'):
         super(RequestZMQ, self).__init__(ip_addr, port)
-        self._socket = ctx.socket(zmq.REQ)
-        print(self.address)
+        self._socket = RequestZMQ.acquire()
+
+    async def __aenter__(self):
         self._socket.connect(f'tcp://{self.address}')
+        return self._socket
 
-    def acquire(self):
-        pass
-
-    def release(self):
-        pass
+    async def __aexit__(self, *exc):
+        self._socket.close()
 
     def __repr__(self):
         return f'<REQ:{self.address}>'
 
+    @staticmethod
+    def acquire():
+        return ctx.socket(zmq.REQ)
+
+    def release(self):
+        if self._socket:
+            self._socket.close()
+
 
 class ResponseZMQ(SocketZMQ):
     def __init__(self, ip_addr='*', port='5556'):
-        self._socket = ctx.socket(zmq.REP)
-        self.binded_addr = f'{ip_addr}:{port}'
-        self._socket.bind(f'tcp://{self.binded_addr}')
+        super(ResponseZMQ, self).__init__(ip_addr, port)
+        self._socket = ResponseZMQ.acquire()
+
+    def __aenter__(self):
+        self._socket.bind(f'tcp://{self.address}')
+        return self._socket
+
+    def __aexit__(self, *exc):
+        self._socket.close()
 
     def __repr__(self):
         return f'<REP:{self.binded_addr}>'
+
+    @staticmethod
+    def acquire():
+        return ctx.socket(zmq.REP)
+
+    def release(self):
+        if self._socket:
+            self._socket.close()
