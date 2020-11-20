b2d7c0064b7baf5e74c858bf1732d34e723fa7af
enjoyintheheat <enjoyintheheat@gmail.com>
2020-11-21 00:51:06 +0300

    Fixing models classes

--- yaqc/__main__.py
+++ yaqc/__main__.py
@@ -3,47 +3,31 @@ import zmq
 import zmq.asyncio
 from contextlib import suppress
 from weakref import WeakSet
+from models.req_rep import RequestZMQ, ResponseZMQ
 
 
-ctx = zmq.asyncio.Context()
+async def sender():
+    with suppress(asyncio.CancelledError):
+        async with RequestZMQ() as req_socket:
+            while True:
+                await req_socket.send_json({'msg': 'hello'})
+                data = await req_socket.recv_json()
+                print(data)
+                await asyncio.sleep(1)
 
 
-class SubscribeController:
-    def __init__(self):
-        self.sockets = {}
-
-    def acquire_socket(topic='main', ip_addr='*', port='5555'):
-        sock = ctx.socket(zmq.SUB)
-        address = f'{ip_addr}:{port}'
-        sock.connect(f'tcp://{address}')
-        sock.setsockopt_string(zmq.SUBSCRIBE, topic)
-        self.sockets.setdefault(topic, [])
-        if self.sockets[topic].get()
-        sock_struct = {f'{address}': sock}
-        self.sockets[topic].append(sock_struct)
-
-
-
-class PipelineController:
-    def __init__(self, max_size=100):
-        self.queue_in = asyncio.Queue(max_size)
-        self.queue_out = asyncio.Queue(max_size)
-
-    async def sub_main_topic(self, name='main', ip_addr='*', port='5555'):
-        self.main_sock = ctx.socket(zmq.SUB)
-        self.main_sock.connect(f'tcp://{ip_addr}:{port}')
-        self.main_sock.setsockopt_string(zmq.SUBSCRIBE, name)
+async def receiver():
+    with suppress(asyncio.CancelledError):
+        async with ResponseZMQ() as rep_socket:
+            while True:
+                data = await rep_socket.recv_json()
+                print(data)
+                await rep_socket.send_json({'code': 200})
+                await asyncio.sleep(1)
 
 
 async def main():
-    sock = ctx.socket(zmq.SUB)
-    sock.connect('tcp://*:5555')
-    sock.setsockopt_string(zmq.SUBSCRIBE, '1')
-    with suppress(asyncio.CancelledError):
-        while True:
-            message = await sock.recv_json()
-            print(message)
-    sock.close()
+    await asyncio.gather(receiver(), sender())
 
 
 if __name__ == '__main__':
@@ -52,4 +36,3 @@ if __name__ == '__main__':
         asyncio.run(main())
     except KeyboardInterrupt:
         print('Finishing...')
-        ctx.term()
--- yaqc/models/req_rep.py
+++ yaqc/models/req_rep.py
@@ -31,15 +31,15 @@ class RequestZMQ(SocketZMQ):
 
 
 class ResponseZMQ(SocketZMQ):
-    def __init__(self, ip_addr='*', port='5556'):
+    def __init__(self, ip_addr='127.0.0.1', port='5555'):
         super(ResponseZMQ, self).__init__(ip_addr, port)
         self._socket = ResponseZMQ.acquire()
 
-    def __aenter__(self):
+    async def __aenter__(self):
         self._socket.bind(f'tcp://{self.address}')
         return self._socket
 
-    def __aexit__(self, *exc):
+    async def __aexit__(self, *exc):
         self._socket.close()
 
     def __repr__(self):
