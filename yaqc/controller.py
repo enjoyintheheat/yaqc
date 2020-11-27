fee45fa0b983a1c0d06a2046cda56c28fb93e070
enjoyintheheat <enjoyintheheat@gmail.com>
2020-11-28 00:30:57 +0300

    Updates Controller class

--- yaqc/controller.py
+++ yaqc/controller.py
@@ -2,4 +2,10 @@ from asyncio import Queue
 
 class Controller:
     def __init__(self, max_size=100):
-        self.queue = Queue(maxsize=max_size)
+        self._queue = Queue(maxsize=max_size)
+
+    async def put(self, elem):
+        await self._queue.put(elem)
+
+    async def get(self):
+        return await self._queue.get()
