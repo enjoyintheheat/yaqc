4b65ab60108e578814145c7932602d0e72a21321
enjoyintheheat <enjoyintheheat@gmail.com>
2020-12-05 02:32:58 +0300

    Added pytest cases

--- tests/test_reqrep.py
+++ tests/test_reqrep.py
@@ -1,10 +1,14 @@
-import unittest
+import pytest
 from yaqc.models.req_rep import RequestZMQ
+from yaqc.models.interfaces import ConnectionState
 
 
-class ReqRepTest(unittest.TestCase):
-    def setUp(self):
-        self.req_zmq = RequestZMQ()
-
-    def test_req_socket(self):
-        print(self.req_zmq)
+@pytest.mark.asyncio
+async def test_requestzmq_instance():
+    req = RequestZMQ()
+    req_detail = req.detail()
+    print(req_detail['name'])
+    assert type(req).__name__ == req_detail['name']
+    assert '127.0.0.1' == req_detail['ip']
+    assert '5555' == req_detail['port']
+    assert ConnectionState.CREATED == req_detail['state']
